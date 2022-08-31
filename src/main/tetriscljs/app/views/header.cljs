25fad7f4a39ee9f59baef3a95a0c219f6ee0e28e
toptobes <96998732+toptobes@users.noreply.github.com>
2022-08-31 10:28:34 -0500

    Idk

--- src/main/tetriscljs/app/views/header.cljs
+++ src/main/tetriscljs/app/views/header.cljs
@@ -4,6 +4,8 @@
 
 (defn Header []
   [:div {:style {:display "flex", :align-items "center"}}
-   [:img {:src cljs-icon :width 120}]
+   [:img {:src cljs-icon :width 120 :style {:filter "contrast(130%)"}}]
    [:h1 {:style {:padding "1rem", :font-weight "bold"}}
-    "Create Reagent App"]])
+    [:span {:style {:text-decoration "line-through"}}
+     "Create Reagent App"]
+    "Tetris soon, I swear"]])
