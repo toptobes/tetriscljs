48e0648f6c7378e8f21773fc227c214541f2ee17
toptobes <96998732+toptobes@users.noreply.github.com>
2022-08-31 11:42:25 -0500

    slight refact

--- src/main/tetriscljs/app/views/background.cljs
+++ src/main/tetriscljs/app/views/background.cljs
@@ -1,5 +1,15 @@
 (ns tetriscljs.app.views.background)
 
+(declare left-gradient right-gradient top-gradient bottom-gradient)
+
+(defn Background [& children]
+  [:div.background
+   [:div {:style left-gradient}]
+   [:div {:style right-gradient}]
+   [:div {:style top-gradient}]
+   [:div {:style bottom-gradient}]
+   children])
+
 (defn gradient-str [rotation]
   (str "linear-gradient(" rotation ", #414141, transparent)"))
 
@@ -14,11 +24,3 @@
 
 (def bottom-gradient
   {:position "absolute" :background-image (gradient-str "to top") :height "5vmax" :width "100%" :bottom 0})
-
-(defn Background [& children]
-  [:div.background
-   [:div {:style left-gradient}]
-   [:div {:style right-gradient}]
-   [:div {:style top-gradient}]
-   [:div {:style bottom-gradient}]
-   children])
--- src/main/tetriscljs/app/views/header.cljs
+++ src/main/tetriscljs/app/views/header.cljs
@@ -2,10 +2,20 @@
 
 (def cljs-icon "/img/cljs.svg")
 
+(declare wrapper header strike-through)
+
 (defn Header []
-  [:div {:style {:display "flex", :align-items "center"}}
+  [:div {:style wrapper}
    [:img {:src cljs-icon :width 120 :style {:filter "contrast(130%)"}}]
-   [:h1 {:style {:padding "1rem", :font-weight "bold"}}
-    [:span {:style {:text-decoration "line-through"}}
-     "Create Reagent App"]
-    "Tetris soon"]])
+   [:h1 {:style header}
+    (strike-through "Create Reagent App") "Tetris soon"]])
+
+(def wrapper
+  {:display "flex", :align-items "center"})
+
+(def header
+  {:padding "1rem", :font-weight "bold"})
+
+(defn strike-through [str]
+  [:span {:style {:text-decoration "line-through"}}
+   str])
